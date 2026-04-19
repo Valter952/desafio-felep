88d35e8b92a6475cf72aa6fcac9b7f25b104872f
Valter952 <dvalter06@gmail.com>
2026-04-19 20:45:04 -0300

    modificação final

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 let NameHero = "Valter"
 
-let XP = 1000
+let XP = 7000
 
-switch(XP){
+    if( XP <= 1000){ console.log("O Herói de nome " + NameHero + " está no nível de Ferro.")}
+    else if (XP >= 1001 && XP<=2000){console.log("O Herói de nome " + NameHero + " está no nível de Bronze.")}
+    else if (XP >= 2001 && XP<=5000){console.log("O Herói de nome " + NameHero + " está no nível de Prata.")}
+    else if (XP >= 6001 && XP<=7000){console.log("O Herói de nome " + NameHero + " está no nível de Ouro.")}
+    else if (XP >= 7001 && XP<=8000){console.log("O Herói de nome " + NameHero + " está no nível de Platina Diamante.")}
+    else if (XP >= 8001 && XP<=9000){console.log("O Herói de nome " + NameHero + " está no nível de Ascendente.")}
+    else if (XP >= 9001 && XP<=10000){console.log("O Herói de nome " + NameHero + " está no nível de Imortal.")}
+    else if (XP >= 10001 ){console.log("O Herói de nome " + NameHero + " está no nível de Radiante.")}
 
-    case XP <= 1000:
-    console.log("O Herói de nome " + NameHero + " está no nível de Ferro")
 
 
 
 
-
-}
 
 
 
