5f9206c2f7dea1657e05dbc799de6ac28a49720a
Valter952 <dvalter06@gmail.com>
2026-04-19 20:01:38 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("ola")
+let NameHero = "Valter"
+
+let XP = 1000
+
+switch(XP){
+
+    case XP <= 1000:
+    console.log("O Herói de nome " + NameHero + " está no nível de Ferro")
+
+
+
+
+
+}
+
+
+
